767da80280537030a3dbcfef80cb989ee661a844
ignacio muñoz <ignan.munoz@duocuc.cl>
2024-11-26 01:35:09 -0300

    cors agregado

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
+const cors = require('cors');
+
+server.use(cors({ origin: '*' }));
+
 
 server.use(middlewares);
 server.use(router);
