819142c61bd79d482a8c4d504804929b9a5c752f
ignacio muñoz <ignan.munoz@duocuc.cl>
2024-11-26 01:12:00 -0300

    Actualización del proyecto: Cambios en ApiGlobal y almacenamiento

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("almacen.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 10000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
+});
